bdf6457e44887bfa93da1bb3b8afded86adedf1d
Adrian Petrescu <adrian@apetre.sc>
2023-04-10 15:30:43 -0400

    scripts: updated backup-docker-secrets script so it works even when the service is scheduled on a non-manager node

--- scripts/backup-docker-secrets.sh
+++ scripts/backup-docker-secrets.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 service_name="backup-all-secrets"
 secret_list=( `docker secret ls --format "{{ .Name }}"` )
@@ -15,14 +15,24 @@ cmd=$cmd" nginx"
 echo $cmd
 $cmd
 
+# Wait for the service to be deployed
+while [ "$(docker service ps --format "{{ .CurrentState }}" --filter desired-state=running $service_name)" | grep -q "Running" ]; do
+  sleep 1
+done
+sleep 10
+
 tar_file="backup_`date +\"%Y%m%d-%H%M%S\"`.tar"
-container_id=`docker ps -f "label=com.docker.swarm.service.name=$service_name" -q`
+service_info=`docker service ps --format "{{ .Node }} {{ .ID }}" --filter desired-state=running $service_name`
+node_name=`echo $service_info | awk '{print $1}'`
+task_id=`echo $service_info | awk '{print $2}'`
+node_address=`docker node inspect --format "{{ .Status.Addr }}" $node_name`
+container_id=$(docker inspect --format '{{.Status.ContainerStatus.ContainerID}}' $task_id)
 
 echo $tar_file
-echo $container_id
+echo $node_name
 
-docker exec -w /usr/local $container_id sh -c "tar -cvf $tar_file -C /run/secrets ."
+docker -H $node_address exec -w /usr/local $container_id sh -c "tar -cvf $tar_file -C /run/secrets ."
 
-docker cp $container_id:/usr/local/$tar_file .
+docker -H $node_address cp $container_id:/usr/local/$tar_file .
 
 docker service rm $service_name
